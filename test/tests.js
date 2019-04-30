5514afa7071633e1b61197e0509f85af0438561f
Claire Cassidy <clacassidy@gmail.com>
2019-04-30 10:38:38 -0700

    created form, created and passed TDD test, created JS file

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,26 @@
+import getApplicant from '../src/get-product';
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+
+test('time to test a function', (assert) => {
     //Arrange
     // Set up your parameters and expectations
+    const expected = {
+        name: 'Claire',
+        meowTime: true,
+        die: 9,
+        lives: 9,
+    };
+
+    const formData = new FormData();
+    formData.set('name', expected.name);
+    formData.set('meow-time', 'yes');
+    formData.set('die', 9);
+    formData.set('lives', 9);
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const applicant = getApplicant(formData);
     //Assert
-    assert.equal(true, false);
+    assert.deepEqual(applicant, expected);
 });
