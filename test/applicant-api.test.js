9b0a28f75d8875d7efe532097ae29bc066b88d94
Claire Cassidy <clacassidy@gmail.com>
2019-05-02 10:18:56 -0700

    reworked applicant api test and applicant api.js for multiple applicants, passed tests

--- test/applicant-api.test.js
+++ test/applicant-api.test.js
@@ -2,15 +2,44 @@ import applicantApi from '../src/applicant-api.js';
 const test = QUnit.test;
 
 test('round-trip applicant', (assert) => {
+    localStorage.removeItem('applicants');
     //Arrange
-    // Set up your parameters and expectations
-    const applicant = { name: '' };
+    const applicant = { name: 'cat' };
 
     //Act 
-    // Call the function you're testing and set the result to a const
     applicantApi.save(applicant);
     const result = applicantApi.get();
     
     //Assert
     assert.deepEqual(result, applicant);
+});
+
+test('no applicants in local storage returns empty array', assert => {
+    localStorage.removeItem('applicants');
+    const expected = [];
+
+    const applicants = applicantApi.getAll();
+
+    assert.deepEqual(applicants, expected);
+
+});
+
+test('two saves return array with two items', (assert) => {
+    localStorage.removeItem('applicants');
+
+    //arrange
+    const applicant1 = { name: 'cat1' };
+    const applicant2 = { name: 'cat2' };
+    const expected = [applicant1, applicant2];
+
+    applicantApi.save(applicant1);
+    applicantApi.save(applicant2);
+
+    //act
+    const applicants = applicantApi.getAll();
+
+    //assert
+    assert.deepEqual(applicants, expected);
+    
+
 });
